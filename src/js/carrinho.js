adcc02d5247eff6aeb4b47cb6be89f0e909c0153
unknown <aranha.game.z@gmail.com>
2021-10-04 19:07:43 -0300

    Teste

--- src/js/carrinho.js
+++ src/js/carrinho.js
@@ -1,7 +1,7 @@
 if (sessionStorage.getItem('CarrinhoTamanho') == null)
     sessionStorage.setItem('CarrinhoTamanho', 0)
 
-function AdicionarProdutoPagina(id, quantidade)
+function AdicionarProdutoPagina(id, quantidade, numero)
 {
     console.log("Criando a produto...")
 
@@ -15,14 +15,27 @@ function AdicionarProdutoPagina(id, quantidade)
     console.log("--------------")
 }
 
+function prepararConteudo(id, quantidade, preco, numero)
+{
+    let valor = parseFloat(quantidade) * parseFloat(preco)
+    let nome = '404'
+
+    if (id == 1)
+        nome = "Banana nanica"
+
+    criarAnuncio(nome, valor, numero)
+}
+
 function EnviarConteudo(numero)
 {
     console.log("Enviando conteúdo...")
 
     let id = sessionStorage.getItem('carrinho-' + String(numero))
     let quantidade = sessionStorage.getItem('quantidade-' + String(numero))
+    let preco = 1.99
 
-    AdicionarProdutoPagina(id, quantidade)
+    //AdicionarProdutoPagina(id, quantidade, numero)
+    prepararConteudo(id, quantidade, preco, numero)
 }
 
 function VerificarCarrinho()
@@ -39,6 +52,45 @@ function VerificarCarrinho()
         x++
     }
 
+}
+
+function criarAnuncio(nome, valor, numero)
+{
+    let origem = document.getElementById("Conteudo")
+    let anuncio = document.createElement('div')
+    let anuncioImagemBackgroud = document.createElement('div')
+    let anuncioImagem = document.createElement('div')
+    let anuncioImagemTamanho = document.createElement('img')
+    let anuncioTitulo = document.createElement('div')
+    let anuncioTituloP = document.createElement('p')
+    let anuncioEmpresa = document.createElement('div')
+    let anuncioEmpresaP = document.createElement('p')
+    let anuncioPreco = document.createElement('div')
+    let anuncioPrecoP = document.createElement('p')
+
+    anuncio.className = 'anuncio'
+    anuncio.id = 'anuncio-' + String(numero)
+    anuncioImagemBackgroud.className = 'anuncio-imagem-background'
+    anuncioImagem.className = 'anuncio-imagem'
+    anuncioImagemTamanho.className = 'anuncio-imagem-tamanho'
+    anuncioImagemTamanho.src = './public/icon/banana.png'
+    anuncioTitulo.className = 'anuncio-titulo'
+    anuncioTituloP.innerHTML = nome
+    anuncioEmpresa.className = 'anuncio-empresa'
+    anuncioEmpresaP.innerHTML = 'Almeida'
+    anuncioPreco.className = 'anuncio-preco'
+    anuncioPrecoP.innerHTML = 'R$: '+ String(valor.toFixed(2))
+
+    origem.appendChild(anuncio)
+    anuncio.appendChild(anuncioImagemBackgroud)
+    anuncio.appendChild(anuncioImagem)
+    anuncioImagem.appendChild(anuncioImagemTamanho)
+    anuncio.appendChild(anuncioTitulo)
+    anuncioTitulo.appendChild(anuncioTituloP)
+    anuncio.appendChild(anuncioEmpresa)
+    anuncioEmpresa.appendChild(anuncioEmpresaP)
+    anuncio.appendChild(anuncioPreco)
+    anuncioPreco.appendChild(anuncioPrecoP)
 }
 
 VerificarCarrinho()
